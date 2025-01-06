e4d620329ea768fb002356df7879302b031ac358
eeuunn <haspring@dsm.hs.kr>
2025-01-07 07:43:35 +0900

    setting

---
+++ src/main/java/com/example/yuagabe/YuagaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.yuagabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuagaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuagaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yuagabe/YuagaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yuagabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuagaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
