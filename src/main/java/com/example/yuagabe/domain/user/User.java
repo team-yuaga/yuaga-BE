1040e0ffaa02fe46f4fc356a761e053c7acbfb57
eeuunn <haspring@dsm.hs.kr>
2025-01-21 17:23:52 +0900

    feat(user) : 유저 엔티티 생성

---
+++ src/main/java/com/example/yuagabe/domain/user/User.java
@@ -0,0 +1,41 @@
+package com.example.yuagabe.domain.user;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tbl_user")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(columnDefinition = "varchar(20)", nullable = false)
+    private String nickName;
+
+    @Column(columnDefinition = "varchar(20)", nullable = false)
+    private String password;
+
+    @Column(columnDefinition = "varchar(6)")
+    private String height;
+
+    @Column(columnDefinition = "varchar(6)")
+    private String weight;
+
+    @Column(columnDefinition = "TEXT")
+    private String skin;
+
+    @Column(columnDefinition = "TEXT")
+    private String style;
+
+    @Column(name = "personal_color", columnDefinition = "varchar(20)")
+    @Enumerated(EnumType.STRING)
+    private String UserPersonalColor;
+}
