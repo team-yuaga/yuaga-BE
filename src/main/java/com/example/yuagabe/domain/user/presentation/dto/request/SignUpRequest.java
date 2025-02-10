a190c2a60f76d05b68fc70f27bd6eadc6ec32f56
eeuunn <haspring@dsm.hs.kr>
2025-02-10 21:14:18 +0900

    feat(user) : 요청 값 수정

--- src/main/java/com/example/yuagabe/domain/user/presentation/dto/request/SignUpRequest.java
+++ src/main/java/com/example/yuagabe/domain/user/presentation/dto/request/SignUpRequest.java
@@ -1,4 +1,4 @@
-package com.example.yuagabe.domain.user.controller;
+package com.example.yuagabe.domain.user.presentation.dto.request;
 
 
 import lombok.AllArgsConstructor;
@@ -12,11 +12,15 @@ import javax.validation.constraints.Size;
 public class SignUpRequest {
     @NotBlank
     @Size(max = 15 , message = "아이디는 최대 15자까지 허용입니다.")
-    private final String id;
+    private final String accountId;
 
     @NotBlank
     @Size(min = 8, max = 20, message = "비밀번호는 최소 8자 ~ 20자까지 허용입니다.")
     private final String password;
+
+    @NotBlank
+    @Size(min = 8, max = 20, message = "비밀번호는 최소 8자 ~ 20자까지 허용입니다.")
+    private final String checkPassword;
 
     @NotBlank
     @Size(max = 10 , message = "닉네임은 최대 10자까지 허용입니다.")
--- src/main/java/com/example/yuagabe/global/security/auth/domain/repository/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.yuagabe.global.security.auth.domain.repository;
-
-
-import com.example.yuagabe.global.security.auth.domain.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-    RefreshToken findByRefreshToken(String refreshToken);
-}
