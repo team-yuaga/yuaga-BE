2f2dd53b4ab5b3f1e7e2dc0597cc21d4c574316f
eeuunn <haspring@dsm.hs.kr>
2025-02-10 21:15:16 +0900

    feat(auth) : securityConfig 추가

---
+++ src/main/java/com/example/yuagabe/global/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.example.yuagabe.global;
+
+import com.example.yuagabe.global.security.jwt.JwtTokenProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .formLogin().disable()
+                .cors()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/v3/api-docs/**").permitAll()
+
+                // user
+                .antMatchers(HttpMethod.POST, "/users/signup").permitAll()
+
+                // feed
+                .antMatchers(HttpMethod.GET, "/feed/list").permitAll()
+                .antMatchers(HttpMethod.GET, "/feed/{feed-id}").permitAll()
+                .antMatchers(HttpMethod.GET, "/feed/search/{title}").permitAll()
+
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new FilterConfig(jwtTokenProvider, objectMapper))
+
+                .and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
