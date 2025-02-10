03597559aa3d6b76f9683cab7d833a76dcb9d57f
eeuunn <haspring@dsm.hs.kr>
2025-02-10 21:14:34 +0900

    feat(user) : 회원가입 controller 추가

---
+++ src/main/java/com/example/yuagabe/domain/user/presentation/UserController.java
@@ -0,0 +1,23 @@
+package com.example.yuagabe.domain.user.presentation;
+
+import com.example.yuagabe.domain.user.presentation.dto.request.SignUpRequest;
+import com.example.yuagabe.domain.user.service.SignUpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    private final SignUpService signUpService;
+
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signup(@RequestBody @Valid SignUpRequest request){
+        signUpService.execute(request);
+    }
+
+}
