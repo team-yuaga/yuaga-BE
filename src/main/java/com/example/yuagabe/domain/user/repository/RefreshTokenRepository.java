f4f7e05cc551d760bf9fa20082340e1eb24871bf
eeuunn <haspring@dsm.hs.kr>
2025-02-10 21:16:31 +0900

    feat(auth) : refreshToken Repository 추가

---
+++ src/main/java/com/example/yuagabe/domain/user/repository/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package com.example.yuagabe.domain.user.repository;
+
+
+import com.example.yuagabe.domain.user.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    RefreshToken findByRefreshToken(String refreshToken);
+}
