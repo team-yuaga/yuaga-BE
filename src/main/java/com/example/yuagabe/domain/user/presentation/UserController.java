b4245310819b7a719ec116236317477534f89333
eeuunn <haspring@dsm.hs.kr>
2025-02-11 00:22:30 +0900

    feat(user) : 닉네임 변경

--- src/main/java/com/example/yuagabe/domain/user/presentation/UserController.java
+++ src/main/java/com/example/yuagabe/domain/user/presentation/UserController.java
@@ -1,8 +1,10 @@
 package com.example.yuagabe.domain.user.presentation;
 
+import com.example.yuagabe.domain.user.presentation.dto.request.NicknameChangeRequest;
 import com.example.yuagabe.domain.user.presentation.dto.request.SignInRequest;
 import com.example.yuagabe.domain.user.presentation.dto.request.SignUpRequest;
 import com.example.yuagabe.domain.user.presentation.dto.response.TokenResponse;
+import com.example.yuagabe.domain.user.service.NicknameChangeService;
 import com.example.yuagabe.domain.user.service.SignInService;
 import com.example.yuagabe.domain.user.service.SignUpService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,7 @@ import javax.validation.Valid;
 public class UserController {
     private final SignUpService signUpService;
     private final SignInService signInService;
+    private final NicknameChangeService nicknameChangeService;
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
@@ -28,5 +31,11 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     public TokenResponse signIn(@RequestBody @Valid SignInRequest request) {
         return signInService.execute(request);
+    }
+
+    @PatchMapping("/nickname")
+    @ResponseStatus(HttpStatus.OK)
+    public void nickname(@RequestBody @Valid NicknameChangeRequest request){
+        nicknameChangeService.execute(request);
     }
 }
