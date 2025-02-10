6db15c726fd5196eb0a71ca44f098e44c055bf0f
eeuunn <haspring@dsm.hs.kr>
2025-02-10 21:46:28 +0900

    feat(user) : signin controller

--- src/main/java/com/example/yuagabe/domain/user/presentation/UserController.java
+++ src/main/java/com/example/yuagabe/domain/user/presentation/UserController.java
@@ -1,10 +1,14 @@
 package com.example.yuagabe.domain.user.presentation;
 
+import com.example.yuagabe.domain.user.presentation.dto.request.SignInRequest;
 import com.example.yuagabe.domain.user.presentation.dto.request.SignUpRequest;
+import com.example.yuagabe.domain.user.presentation.dto.response.TokenResponse;
+import com.example.yuagabe.domain.user.service.SignInService;
 import com.example.yuagabe.domain.user.service.SignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
@@ -12,11 +16,17 @@ import javax.validation.Valid;
 @RestController
 public class UserController {
     private final SignUpService signUpService;
+    private final SignInService signInService;
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
-    public void signup(@RequestBody @Valid SignUpRequest request){
+    public void signup(@RequestBody @Valid SignUpRequest request) {
         signUpService.execute(request);
     }
 
+    @PostMapping("/signin")
+    @ResponseStatus(HttpStatus.OK)
+    public TokenResponse signIn(@RequestBody @Valid SignInRequest request) {
+        return signInService.execute(request);
+    }
 }
