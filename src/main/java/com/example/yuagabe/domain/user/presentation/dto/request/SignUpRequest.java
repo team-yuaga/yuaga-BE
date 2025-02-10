630f48ade2102de4d1659fdc68c3179745a00172
eeuunn <haspring@dsm.hs.kr>
2025-02-11 00:22:20 +0900

    feat(user) : request 추가 및 수정

--- src/main/java/com/example/yuagabe/domain/user/presentation/dto/request/SignUpRequest.java
+++ src/main/java/com/example/yuagabe/domain/user/presentation/dto/request/SignUpRequest.java
@@ -1,26 +1,27 @@
 package com.example.yuagabe.domain.user.presentation.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class SignUpRequest {
     @NotBlank
     @Size(max = 15 , message = "아이디는 최대 15자까지 허용입니다.")
-    private final String accountId;
+    private String accountId;
 
     @NotBlank
     @Size(min = 8, max = 20, message = "비밀번호는 최소 8자 ~ 20자까지 허용입니다.")
-    private final String password;
+    private String password;
 
     @NotBlank
     @Size(min = 8, max = 20, message = "비밀번호는 최소 8자 ~ 20자까지 허용입니다.")
-    private final String checkPassword;
+    private String checkPassword;
 
     @NotBlank
     @Size(max = 10 , message = "닉네임은 최대 10자까지 허용입니다.")
-    private final String nickname;
+    private String nickname;
 }
