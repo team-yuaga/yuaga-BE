1cf28ed9f513fe566f99d9614d2de17590e482c6
eeuunn <haspring@dsm.hs.kr>
2025-02-17 00:38:02 +0900

    feat(user) : jsonproperty 추가

--- src/main/java/com/example/yuagabe/domain/user/presentation/dto/request/SignUpRequest.java
+++ src/main/java/com/example/yuagabe/domain/user/presentation/dto/request/SignUpRequest.java
@@ -1,5 +1,6 @@
 package com.example.yuagabe.domain.user.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import javax.validation.constraints.Size;
 public class SignUpRequest {
     @NotBlank
     @Size(max = 15 , message = "아이디는 최대 15자까지 허용입니다.")
+    @JsonProperty("accountId")
     private String accountId;
 
     @NotBlank
@@ -19,6 +21,7 @@ public class SignUpRequest {
 
     @NotBlank
     @Size(min = 8, max = 20, message = "비밀번호는 최소 8자 ~ 20자까지 허용입니다.")
+    @JsonProperty("checkPassword")
     private String checkPassword;
 
     @NotBlank
