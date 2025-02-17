e45377ad8c3df06a79fe8c9d35a8b4303aa6b7b5
eeuunn <haspring@dsm.hs.kr>
2025-02-17 21:42:12 +0900

    feat(user) : JPA로 변경

--- src/main/java/com/example/yuagabe/domain/user/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/yuagabe/domain/user/domain/repository/RefreshTokenRepository.java
@@ -1,10 +1,8 @@
 package com.example.yuagabe.domain.user.domain.repository;
 
 import com.example.yuagabe.domain.user.domain.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
     RefreshToken findByRefreshToken(String refreshToken);
 }
--- src/main/java/com/example/yuagabe/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/yuagabe/domain/user/domain/repository/UserRepository.java
@@ -1,13 +1,11 @@
 package com.example.yuagabe.domain.user.domain.repository;
 
 import com.example.yuagabe.domain.user.domain.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByAccountId(String accountId);
 
 }
