d9750b49a40ef0a57de7e723f65e20fe8011c50d
eeuunn <haspring@dsm.hs.kr>
2025-02-18 01:08:19 +0900

    feat(feed) : 게시물 생성 서비스 추가

---
+++ src/main/java/com/example/yuagabe/domain/feed/service/CreateFeedService.java
@@ -0,0 +1,31 @@
+package com.example.yuagabe.domain.feed.service;
+
+
+import com.example.yuagabe.domain.feed.domain.Feed;
+import com.example.yuagabe.domain.feed.domain.repository.FeedRepository;
+import com.example.yuagabe.domain.feed.presentation.dto.request.CreateFeedRequest;
+import com.example.yuagabe.domain.user.domain.User;
+import com.example.yuagabe.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CreateFeedService {
+    private final FeedRepository feedRepository;
+    private final UserFacade userFacade;
+
+    @Transactional
+    public void execute(CreateFeedRequest request) {
+        User user = userFacade.getCurrentUser();
+
+        feedRepository.save(Feed.builder()
+                .feedLocal(request.getFeedLocal())
+                .title(request.getTitle())
+                .tags(request.getTags())
+                .season(request.getSeason())
+                .user(user)
+                .build());
+    }
+}
