34da0c16716cef40649da831f7c7d18114e93532
eeuunn <haspring@dsm.hs.kr>
2025-02-18 02:02:21 +0900

    feed(feed) : 게시물 삭제 controller

--- src/main/java/com/example/yuagabe/domain/feed/presentation/FeedController.java
+++ src/main/java/com/example/yuagabe/domain/feed/presentation/FeedController.java
@@ -1,8 +1,8 @@
 package com.example.yuagabe.domain.feed.presentation;
 
-
 import com.example.yuagabe.domain.feed.presentation.dto.request.CreateFeedRequest;
 import com.example.yuagabe.domain.feed.service.CreateFeedService;
+import com.example.yuagabe.domain.feed.service.DeleteFeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +14,17 @@ import javax.validation.Valid;
 @RestController
 public class FeedController {
     private final CreateFeedService createFeedService;
+    private final DeleteFeedService deleteFeedService;
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public void createFeed(@RequestBody @Valid CreateFeedRequest request){
+    public void createFeed(@RequestBody @Valid CreateFeedRequest request) {
         createFeedService.execute(request);
+    }
+
+    @DeleteMapping("/{feed-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFeed(@PathVariable("feed-id") Long feedId) {
+        deleteFeedService.execute(feedId);
     }
 }
