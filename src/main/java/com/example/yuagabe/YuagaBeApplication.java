b8b2b2eb45150c1a4fa22a0b9e68c990bde078b6
eeuunn <haspring@dsm.hs.kr>
2025-02-17 20:39:23 +0900

    chore : update

--- src/main/java/com/example/yuagabe/YuagaBeApplication.java
+++ src/main/java/com/example/yuagabe/YuagaBeApplication.java
@@ -3,9 +3,10 @@ package com.example.yuagabe;
 import com.example.yuagabe.global.security.jwt.JwtProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableConfigurationProperties(JwtProperty.class)
 public class YuagaBeApplication {
 
