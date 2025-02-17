367ec6b558c2578054247295c2db586280b5f0ca
eeuunn <haspring@dsm.hs.kr>
2025-02-17 21:39:14 +0900

    feat(user) : repository 수정

--- src/main/java/com/example/yuagabe/domain/user/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/yuagabe/domain/user/domain/repository/RefreshTokenRepository.java
@@ -2,7 +2,9 @@ package com.example.yuagabe.domain.user.domain.repository;
 
 import com.example.yuagabe.domain.user.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
     RefreshToken findByRefreshToken(String refreshToken);
 }
--- src/main/java/com/example/yuagabe/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/yuagabe/domain/user/domain/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.example.yuagabe.domain.user.domain.repository;
 
 import com.example.yuagabe.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByAccountId(String accountId);
 
