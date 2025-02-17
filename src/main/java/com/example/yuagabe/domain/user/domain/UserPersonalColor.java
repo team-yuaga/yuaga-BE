df536235ffa3c07477b1c30e693a27d69f3817e9
eeuunn <haspring@dsm.hs.kr>
2025-02-18 01:08:40 +0900

    feat(feed) : enum 한글 매핑 추가

--- src/main/java/com/example/yuagabe/domain/user/domain/UserPersonalColor.java
+++ src/main/java/com/example/yuagabe/domain/user/domain/UserPersonalColor.java
@@ -1,5 +1,10 @@
 package com.example.yuagabe.domain.user.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+
 public enum UserPersonalColor {
     SPRING("봄웜"),
     SUMMER("여름쿨"),
@@ -13,6 +18,15 @@ public enum UserPersonalColor {
         this.userPersonalColor = userPersonalColor;
     }
 
+    @JsonCreator
+    public static UserPersonalColor from(String value) {
+        return Arrays.stream(UserPersonalColor.values())
+                .filter(color -> color.userPersonalColor.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 userPersonalColor 값입니다: " + value));
+    }
+
+    @JsonValue
     public String getUserPersonalColor() {
         return userPersonalColor;
     }
