88d3971ccd17dede4780ee0e52805fe0cd6243c3
eeuunn <haspring@dsm.hs.kr>
2025-02-18 23:32:01 +0900

    feat(feed) : getFeedService 추가

---
+++ src/main/java/com/example/yuagabe/domain/feed/service/GetFeedService.java
@@ -0,0 +1,22 @@
+package com.example.yuagabe.domain.feed.service;
+
+import com.example.yuagabe.domain.feed.domain.Feed;
+import com.example.yuagabe.domain.feed.facade.FeedFacade;
+import com.example.yuagabe.domain.feed.presentation.dto.response.GetFeedResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class GetFeedService {
+    private final FeedFacade feedFacade;
+
+    @Transactional
+    public List<GetFeedResponse> execute() {
+        List<Feed> feeds = feedFacade.getAllFeed();
+        return feeds.stream().map(feed -> new GetFeedResponse(feed.getTitle(), feed.getTags(), feed.getCreatedAt())).toList();
+    }
+}
