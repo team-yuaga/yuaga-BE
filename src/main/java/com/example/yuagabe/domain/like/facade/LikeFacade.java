c3861b53c072fb5e3bb1cc98a1f19e5f932dc2a1
eeuunn <haspring@dsm.hs.kr>
2025-02-23 18:33:49 +0900

    feat(feed) : likeFacade 매서드 추가

--- src/main/java/com/example/yuagabe/domain/like/facade/LikeFacade.java
+++ src/main/java/com/example/yuagabe/domain/like/facade/LikeFacade.java
@@ -1,6 +1,8 @@
 package com.example.yuagabe.domain.like.facade;
 
+import com.example.yuagabe.domain.feed.domain.Feed;
 import com.example.yuagabe.domain.like.domain.repository.LikeRepository;
+import com.example.yuagabe.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,5 +13,9 @@ public class LikeFacade {
 
     public Long countLikesByFeedId(Long feedId) {
         return likeRepository.countByFeedId(feedId);
+    }
+
+    public Boolean likeBoolean(Feed feedId, User userId){
+        return likeRepository.existsByFeedAndUser(feedId, userId);
     }
 }
