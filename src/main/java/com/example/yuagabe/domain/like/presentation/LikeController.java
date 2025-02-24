527824144b0afc2d7b2ea3c1202f0cf2563b13da
eeuunn <haspring@dsm.hs.kr>
2025-02-24 19:59:46 +0900

    feat(like) : 내가 좋아요 누른 게시물 controller

--- src/main/java/com/example/yuagabe/domain/like/presentation/LikeController.java
+++ src/main/java/com/example/yuagabe/domain/like/presentation/LikeController.java
@@ -1,11 +1,16 @@
 package com.example.yuagabe.domain.like.presentation;
 
 
+import com.example.yuagabe.domain.feed.presentation.dto.response.GetFeedResponse;
+import com.example.yuagabe.domain.like.presentation.dto.GetMyLikeListResponse;
 import com.example.yuagabe.domain.like.service.CreateLikeService;
 import com.example.yuagabe.domain.like.service.DeleteLikeService;
+import com.example.yuagabe.domain.like.service.GetMyLikeListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/likes")
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class LikeController {
     private final CreateLikeService createLikeService;
     private final DeleteLikeService deleteLikeService;
+    private final GetMyLikeListService getMyLikeListService;
 
     @PostMapping("/{feed-id}")
     @ResponseStatus(HttpStatus.OK)
@@ -24,6 +30,12 @@ public class LikeController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteLike(@PathVariable("feed-id") Long feedId){
         deleteLikeService.execute(feedId);
+    }
+
+    @GetMapping("/{user-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<GetMyLikeListResponse> getMyLikeListFeed(@PathVariable("user-id") Long userId){
+        return getMyLikeListService.execute(userId);
     }
 
 }
